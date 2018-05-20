6cdee2116102c31fc1cece1c40ae3227e47b2814
mikkokoivusaari <koivusaarimikko@gmail.com>
2018-05-20 15:54:50 +0300

    Some new classes & UI start

--- src/main/java/TaskAutomater/Client/Servlets/addUser.java
+++ src/main/java/TaskAutomater/Client/Servlets/addUser.java
@@ -2,7 +2,9 @@ package TaskAutomater.Client.Servlets;
 
 import java.io.IOException;
 import javax.*;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 //import javax.servlet.ServletException;
 //import javax.servlet.http.HttpServlet;
 //import javax.servlet.http.HttpServletRequest;
@@ -11,9 +13,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import TaskAutomater.Converter.UserConverter;
+import TaskAutomater.Model.UserModel;
+
 /**
  * Servlet implementation class addUser
  */
+@WebServlet("/addUser")
 public class addUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -37,6 +43,27 @@ public class addUser extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String uname, firstName, lastName, email;
+		uname = request.getParameter("uname");
+		firstName = request.getParameter("firstName");
+		lastName = request.getParameter("lastName");
+		email  = request.getParameter("email");
+		if ((uname == null || uname.equals(""))
+				|| (email == null || email.equals(""))) {
+			request.setAttribute("error", "Mandatory Parameters Missing");
+			RequestDispatcher rd = getServletContext().getRequestDispatcher(
+					"/users.jsp");
+			rd.forward(request, response);
+		} else {
+			UserModel user = new UserModel();
+			user.setEmail(email);
+			user.setFname(firstName);
+			user.setLname(lastName);
+			user.setUname(uname);
+			UserConverter conv = new UserConverter();
+			//insert this user to mongo
+			conv.toUserModel(user);
+		}
 		doGet(request, response);
 	}
 
---
+++ src/main/java/TaskAutomater/DataAccess/UserManagement.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+// 19 May 2018
+package TaskAutomater.DataAccess;
+
+/**
+ * @author mikko
+ * This is just for refactoring code. To use dataaccess more controlled mannor. I think these should be the methods
+ *  servlets use
+ */
+public class UserManagement {
+	//adduser 
+	
+	//return type: boolean
+	public void addUser() {
+		
+	}
+	//remover 
+	//return type: boolean
+	//Must be either current user or user with access rights
+	public void deleteUser() {
+			
+	}
+	//end user validtoDate =  
+	//Return type: Boolean? 
+	public void endUser() {
+					
+	}
+	//This is important. And should be thought properly
+	//Return type UUID
+	public void getSelectedUserGUID() {
+		
+	}
+	//This is for listing users
+	//Return type: listIterable
+	public void getAllUsers() {
+		
+	}
+	//Might be unnecessary
+	public void getActiveUsers() {
+		
+	}
+	
+}
+
